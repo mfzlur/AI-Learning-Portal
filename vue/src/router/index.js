b15b3efcf98241ff4fe08d61d481fb8eaba57251
mpshriveena <132072314+mpshriveena@users.noreply.github.com>
2025-02-19 02:42:19 +0530

    Update index.js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import CourseNavigationPage from '../views/CourseNavigationPage.vue'
+import Courses from '../views/Courses.vue'
+import Feedback from '../views/Feedback.vue'
+import Profile from '../views/Profile.vue'
+import GradedAssignmentPage from '../views/GradedAssignmentPage.vue'
+import ProgrammingAssignmentPage from '../views/ProgrammingAssignmentPage.vue'
+import EachCourse from '../views/EachCourse.vue'
+import InstructorDashboardPage from '../views/InstructorDashboardPage.vue'
+import DeveloperDashboardPage from '../views/DeveloperDashboardPage.vue'
+import SubjectSelectionPage from '../views/SubjectSelectionPage.vue'
 import { useAuthStore } from '../stores/auth'
 import AIAssistantView from '../views/AIAssistantView.vue'
 const router = createRouter({
@@ -26,7 +36,67 @@ const router = createRouter({
       name: 'AIAssistant',
       component: AIAssistantView,
       meta: { requiresAuth: true }
-    }
+    },
+    {
+      path: '/my-courses',
+      name: 'Courses',
+      component: Courses,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/course1',
+      name: 'CourseNavigationPage',
+      component: CourseNavigationPage,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/feedback',
+      name: 'Feedback',
+      component: Feedback,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/ga',
+      name: 'GradedAssignmentPage',
+      component: GradedAssignmentPage,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/pa',
+      name: 'ProgrammingAssignmentPage',
+      component: ProgrammingAssignmentPage,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/topic',
+      name: 'EachCourse',
+      component: EachCourse,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/instructor',
+      name: 'InstructorDashboardPage',
+      component: InstructorDashboardPage,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/developer',
+      name: 'DeveloperDashboardPage',
+      component: DeveloperDashboardPage,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/select-subject',
+      name: 'SubjectSelectionPage',
+      component: SubjectSelectionPage,
+      meta: { requiresAuth: true }
+    },
   ]
 })
 
